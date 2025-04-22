17ab955cc66a89a13147e45c265aff7b300db4ee
lanyahood4 <112507739+lanyahood4@users.noreply.github.com>
2025-04-22 18:20:29 -0400

    earth quake counts

--- analyze_data.py
+++ analyze_data.py
@@ -24,4 +24,17 @@ for city, data in city_data.items():
     avg_humidity = sum(data['humidities']) / len(data['humidities'])
     avg_aqi = sum(data['aqis']) / len(data['aqis'])
     results.append((city, avg_temp, avg_humidity, avg_aqi))
+# Get earthquake counts by general region (e.g., California, Alaska)
+cur.execute('''
+SELECT place FROM Earthquakes
+''')
+quake_rows = cur.fetchall()
+quake_counts = {}
+for place_tuple in quake_rows:
+    place = place_tuple[0]
+    region = place.split(",")[-1].strip() if "," in place else place.split(" ")[-1]
+    if region not in quake_counts:
+        quake_counts[region] = 0
+    quake_counts[region] += 1
+    
 
