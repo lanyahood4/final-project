c85bc301fab9d3847a44281b4d11f70684e4bc39
lanyahood4 <112507739+lanyahood4@users.noreply.github.com>
2025-04-22 18:17:03 -0400

    Update analyze_data.py

--- analyze_data.py
+++ analyze_data.py
@@ -1,2 +1,18 @@
 #Who is doing this one?: 
 import sqlite3
+conn = sqlite3.connect('final_project.db')
+cur = conn.cursor()
+
+# Weather & AQI JOIN
+query = '''
+SELECT 
+    Weather.city,
+    Weather.temperature,
+    Weather.humidity,
+    AirQuality.aqi
+FROM Weather
+JOIN AirQuality
+ON Weather.city = AirQuality.city AND Weather.date = AirQuality.date
+'''
+cur.execute(query)
+rows = cur.fetchall()
