7763c1a1ec0a338946e4303d2ec961f578dc2cbc
lanyahood4 <112507739+lanyahood4@users.noreply.github.com>
2025-04-22 18:38:32 -0400

    Weather & AQI Join

--- visualize.py
+++ visualize.py
@@ -1,3 +1,19 @@
 # Who is doing this one?: 
 import matplotlib.pyplot as plt
 import sqlite3
+
+conn = sqlite3.connect('final_project.db')
+cur = conn.cursor()
+
+# Weather & AQI Join
+cur.execute('''
+SELECT 
+    Weather.city,
+    AVG(Weather.temperature),
+    AVG(Weather.humidity),
+    AVG(AirQuality.aqi)
+FROM Weather
+JOIN AirQuality
+ON Weather.city = AirQuality.city AND Weather.date = AirQuality.date
+GROUP BY Weather.city
+''')
