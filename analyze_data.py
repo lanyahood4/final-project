b340caf9a028a39d0e7eef4c5305bfb51bcdbca5
lanyahood4 <112507739+lanyahood4@users.noreply.github.com>
2025-04-22 18:18:34 -0400

    calculatw weather

--- analyze_data.py
+++ analyze_data.py
@@ -16,3 +16,12 @@ ON Weather.city = AirQuality.city AND Weather.date = AirQuality.date
 '''
 cur.execute(query)
 rows = cur.fetchall()
+# Calculate weather/AQI averages
+results = []
+
+for city, data in city_data.items():
+    avg_temp = sum(data['temps']) / len(data['temps'])
+    avg_humidity = sum(data['humidities']) / len(data['humidities'])
+    avg_aqi = sum(data['aqis']) / len(data['aqis'])
+    results.append((city, avg_temp, avg_humidity, avg_aqi))
+
