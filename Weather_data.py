68e21f985d241d8499eafae6e02b373380041901
lanyahood4 <112507739+lanyahood4@users.noreply.github.com>
2025-04-22 11:54:04 -0400

    1. Function call

--- Weather_data.py
+++ Weather_data.py
@@ -24,6 +24,19 @@ CREATE TABLE IF NOT EXISTS AirQuality (
 ''')
 
 # List of cities to collect AQI data for
-cities = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix','Detroit', 'New Orleans', 'Atlanta']
+cities = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix', 'Detroit', 'New Orleans', 'Atlanta']
 
+# Function to call the WeatherAPI and return relevant data
+def fetch_weather(city):
+    url = f"http://api.weatherapi.com/v1/history.json?key={API_KEY}&q={city}&dt={DATE}"
+    r = requests.get(url)
+    data = r.json()
+    day_data = data['forecast']['forecastday'][0]['day']
+    return (
+        city,
+        DATE,
+        day_data['avgtemp_f'],
+        day_data['avghumidity'],
+        day_data['maxwind_mph']
+    )
 
