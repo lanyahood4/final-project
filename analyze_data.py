c0fb0408f5ea02c0a20439470e176244053b81f6
lanyahood4 <112507739+lanyahood4@users.noreply.github.com>
2025-04-23 11:55:12 -0400

    correct types

--- analyze_data.py
+++ analyze_data.py
@@ -8,3 +8,7 @@ conn = sqlite3.connect("final_project.db")
 # Read tables
 weather_df = pd.read_sql_query("SELECT * FROM Weather", conn)
 earthquake_df = pd.read_sql_query("SELECT * FROM Earthquakes", conn)
+# Ensure correct types
+weather_df['temperature'] = pd.to_numeric(weather_df['temperature'], errors='coerce')
+weather_df['humidity'] = pd.to_numeric(weather_df['humidity'], errors='coerce')
+weather_df['wind_speed'] = pd.to_numeric(weather_df['wind_speed'], errors='coerce')
